11ce1168e98b6b6a3659367cb25ba2c4abc18463
Luca-Rubino <lucasrubino92@gmail.com>
2024-06-28 18:24:02 +0200

    Base scaffolding

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php 
+
+require __DIR__ '/src/login.php';
+include __DIR__ '/src/logout.php';
+include __DIR__ '/src/functions.php';
+
+header( __DIR__ '/login.php');
+exit;
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>php-strong-password-generator</title>
+</head>
+<body>
+    <header></header>
+    <main></main>
+    <footer></footer>
+</body>
+</html>
