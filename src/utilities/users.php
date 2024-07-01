1cda081af2414d8d2378455ea9b5a60c1b52033b
Luca-Rubino <lucasrubino92@gmail.com>
2024-07-01 10:55:27 +0200

    Creazione pagina con array users

---
+++ src/utilities/users.php
@@ -0,0 +1,18 @@
+<?php
+
+$users = [
+    [
+        'username' => 'Luca Rubino',
+        'userpassword' => '1234'
+    ],
+    [
+        'username' => 'Alberto Maggi',
+        'userpassword' => '1234'
+    ],
+    [
+        'username' => 'Carla Forenzi',
+        'userpassword' => '1234'
+    ]
+];
+
+?>
